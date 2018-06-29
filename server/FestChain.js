b60ea7d30f33f45578deddab2d41c7b61cc75244
Rostyslav_Stakhiv <rostyk96@gmail.com>
2018-06-29 19:49:49 +0300

    - Working on SC: added new methods and properties

--- server/FestChain.js
+++ server/FestChain.js
@@ -1,30 +1,86 @@
-class FestChain {
+class FestChainTickets {
 	constructor() {
-
 		// define fields stored to state trie
 		LocalContractStorage.defineProperties(this, {
 			eventsCount: null,
 		});
 
 		LocalContractStorage.defineMapProperties(this, {
-            eventsMapById: null,
-        });
+			eventsMapById: null,
+			ticketOwnersMapById: null,
+		});
 	}
 
 	init() {
-        this.eventsMapById = new BigNumber(0);
+		this.eventsCount = new BigNumber(0);
 	}
 	
-	//Get functions
-	getNextEventId() {
-        return {
-            id: new BigNumber(this.eventsCount).plus(1)
-        };
+	//Public methods
+	buyTicket(eventId) {
+		let buyerId = Blockchain.transaction.from;
+		let ticketOwner = this.ticketOwnersMapById.get(buyerId);
+
+		let ticket = this._createTicket(eventId);
+
+		if(!ticketOwner) {
+			ticketOwner = {
+				id: buyerId,
+				ticketIds: new Set(),
+			}
+		}
+
+		ticketOwner.ticketIds.add(ticket.id);
+		this.ticketOwnersMapById.set(buyerId, ticketOwner);
+	}
+
+	//Create
+    	createEvent(title, description, maxNumberOfTickets) {
+		let eventId = _nextEventID();
+		let creatorId = Blockchain.transaction.from;
+
+		let createdEvent = {
+            		id: eventId,
+            		creatorId: creatorId,
+            		title: title,
+			description: description,
+			maxNumberOfTickets: maxNumberOfTickets,
+			ticketsSold: 0,
+			creationDate: Date.now(),
+		};
+		
+		this.eventsMapById.set(eventId, createdEvent)
+		this.eventsCount = eventId;
+	}
+
+	//Get
+	getAllEvents() {
+		let res = [];
+		for(let i = 1; i <= this.eventsCount; ++i) {
+			let event = this.eventsMapById.get(i);
+			res.push(event);
+		}
+
+		return res;
 	}
 	
-	//Create functions
-    createEvent(title, description) {
-    }
+	//Private methods
+	_pay(to, amount) {
+        	return Blockchain.transfer(to, amount)
+	}
+	
+	_nextEventID() {
+		return new BigNumber(this.eventsCount.plus(1));
+	}
+
+	_createTicket(eventId) {
+		//Code to create ticket token
+		let ticket = {
+			id: 0,
+			eventId: eventId,
+		}
+
+		return ticket;
+	}
 }
 
-module.exports = FestChain;
+module.exports = FestChainTickets;
